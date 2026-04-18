3c003ad43cfc0ccebf265c3a2db1ccd3c88a881b
martha-jauregui <martha.jauregui.550@my.csun.edu>
2026-04-18 00:00:37 -0700

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function() { 
-    // Requirement: Initialize the pet's information in the HTML [cite: 8]
+    // Requirement: Initialize the pet's information in the HTML
     checkAndUpdatePetInfoInHtml("Welcome! I'm ready to hang out!");
   
     $('.treat-button').click(clickedTreatButton);
@@ -8,21 +8,21 @@ $(function() {
     $('.nap-button').click(clickedNapButton);
 });
 
-// Requirement: Create pet_info object with name, weight, and happiness [cite: 8]
+// Requirement: Create pet_info object with name, weight, and happiness
 var pet_info = {name: "Mochi", weight: 15, happiness: 10};
 
 function clickedTreatButton() {
     $('.pet-image').fadeOut(100).fadeIn(100);
     $('#munch-sound').get(0).play();
 
-    // Requirement: Add to happiness and weight [cite: 9]
-    pet_info.happiness += 5;
+    // Requirement: Add to happiness and weight 
+    pet_info.happiness += 5;S
     pet_info.weight += 2;
     checkAndUpdatePetInfoInHtml("Yum! That treat was delicious!");
 }
 
 function clickedPlayButton() {
-    // Requirement: Add to happiness and reduce weight [cite: 10]
+    // Requirement: Add to happiness and reduce weight 
      $('.pet-image').fadeOut(100).fadeIn(100);
     pet_info.happiness += 10;
     pet_info.weight -= 5;
@@ -30,7 +30,7 @@ function clickedPlayButton() {
 }
 
 function clickedExerciseButton() {
-    // Requirement: Reduce happiness and weight [cite: 10]
+    // Requirement: Reduce happiness and weight 
     $('.pet-image').fadeOut(100).fadeIn(100);
     pet_info.happiness -= 5;
     pet_info.weight -= 5;
@@ -44,14 +44,13 @@ function clickedNapButton() {
     checkAndUpdatePetInfoInHtml("Zzz... I feel refreshed.");
 }
 
-// FIX: Added 'comment' as a parameter so it can travel to the update function
 function checkAndUpdatePetInfoInHtml(comment) {
     checkWeightAndHappinessBeforeUpdating();  
     updatePetInfoInHtml(comment);
 }
 
 function checkWeightAndHappinessBeforeUpdating() {
-    // Requirement: Fix bugs so values can't go below zero [cite: 11]
+    // Requirement: Fix bugs so values can't go below zero 
     if (pet_info.weight < 0) {
         pet_info.weight = 0;
     }
@@ -60,20 +59,19 @@ function checkWeightAndHappinessBeforeUpdating() {
     }
 }
 
-// FIX: This function now correctly accepts 'comment' as a parameter
 function updatePetInfoInHtml(comment) {
     $('.name').text(pet_info['name']);
     $('.weight').text(pet_info['weight']);
     $('.happiness').text(pet_info['happiness']);
     
-    /* UNIQUE METHOD 1: .slideToggle() [cite: 17]
-       EXPLANATION: This animates the element's height. We hide the text first
-       then slide it in so the visual notification appears without an alert[cite: 15, 18].
+    /* UNIQUE METHOD 1: .slideToggle()
+       This animates the element's height. We hide the text first
+       then slide it in so the visual notification appears without an alert
     */
     $('#pet-comment').hide().text(comment).slideToggle(300);
 
-    /* UNIQUE METHOD 2: .width() [cite: 17]
-       EXPLANATION: This sets the width of the element. We use it to make the 
+    /* UNIQUE METHOD 2: .width()
+       This sets the width of the element. We use it to make the 
        image size change based on the pet's weight property.
     */
     var dynamicWidth = 150 + (pet_info.weight * 2);
