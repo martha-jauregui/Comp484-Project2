1fe5125ba2b09cef9235f52f4fdc98b7491bb69e
martha-jauregui <martha.jauregui.550@my.csun.edu>
2026-04-17 23:52:41 -0700

    finalized version

--- script.js
+++ script.js
@@ -1,52 +1,81 @@
-$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
-    
-    // Called function to update the name, happiness, and weight of our pet in our HTML
-    checkAndUpdatePetInfoInHtml();
+$(function() { 
+    // Requirement: Initialize the pet's information in the HTML [cite: 8]
+    checkAndUpdatePetInfoInHtml("Welcome! I'm ready to hang out!");
   
-    // When each button is clicked, it will "call" function for that button (functions are below)
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-  
+    $('.nap-button').click(clickedNapButton);
+});
 
-  
+// Requirement: Create pet_info object with name, weight, and happiness [cite: 8]
+var pet_info = {name: "Mochi", weight: 15, happiness: 10};
+
+function clickedTreatButton() {
+    $('.pet-image').fadeOut(100).fadeIn(100);
+    $('#munch-sound').get(0).play();
+
+    // Requirement: Add to happiness and weight [cite: 9]
+    pet_info.happiness += 5;
+    pet_info.weight += 2;
+    checkAndUpdatePetInfoInHtml("Yum! That treat was delicious!");
+}
+
+function clickedPlayButton() {
+    // Requirement: Add to happiness and reduce weight [cite: 10]
+     $('.pet-image').fadeOut(100).fadeIn(100);
+    pet_info.happiness += 10;
+    pet_info.weight -= 5;
+    checkAndUpdatePetInfoInHtml("I love playing with you!");
+}
+
+function clickedExerciseButton() {
+    // Requirement: Reduce happiness and weight [cite: 10]
+    $('.pet-image').fadeOut(100).fadeIn(100);
+    pet_info.happiness -= 5;
+    pet_info.weight -= 5;
+    checkAndUpdatePetInfoInHtml("Whew! That was a ruff workout.");
+}
+
+function clickedNapButton() { 
+    // Requirement: Add a new action and behavior 
+     $('.pet-image').fadeOut(100).fadeIn(100);
+    pet_info.happiness += 2;
+    checkAndUpdatePetInfoInHtml("Zzz... I feel refreshed.");
+}
+
+// FIX: Added 'comment' as a parameter so it can travel to the update function
+function checkAndUpdatePetInfoInHtml(comment) {
+    checkWeightAndHappinessBeforeUpdating();  
+    updatePetInfoInHtml(comment);
+}
+
+function checkWeightAndHappinessBeforeUpdating() {
+    // Requirement: Fix bugs so values can't go below zero [cite: 11]
+    if (pet_info.weight < 0) {
+        pet_info.weight = 0;
+    }
+    if (pet_info.happiness < 0) {
+        pet_info.happiness = 0;
+    }
+}
+
+// FIX: This function now correctly accepts 'comment' as a parameter
+function updatePetInfoInHtml(comment) {
+    $('.name').text(pet_info['name']);
+    $('.weight').text(pet_info['weight']);
+    $('.happiness').text(pet_info['happiness']);
     
-  })
-  
-    // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
-  
-    function clickedTreatButton() {
-      // Increase pet happiness
-      // Increase pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedPlayButton() {
-      // Increase pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedExerciseButton() {
-      // Decrease pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-  
-    function checkAndUpdatePetInfoInHtml() {
-      checkWeightAndHappinessBeforeUpdating();  
-      updatePetInfoInHtml();
-    }
-    
-    function checkWeightAndHappinessBeforeUpdating() {
-      // Add conditional so if weight is lower than zero.
-    }
-    
-    // Updates your HTML with the current values in your pet_info object
-    function updatePetInfoInHtml() {
-      $('.name').text(pet_info['name']);
-      $('.weight').text(pet_info['weight']);
-      $('.happiness').text(pet_info['happiness']);
-    }
-  
+    /* UNIQUE METHOD 1: .slideToggle() [cite: 17]
+       EXPLANATION: This animates the element's height. We hide the text first
+       then slide it in so the visual notification appears without an alert[cite: 15, 18].
+    */
+    $('#pet-comment').hide().text(comment).slideToggle(300);
+
+    /* UNIQUE METHOD 2: .width() [cite: 17]
+       EXPLANATION: This sets the width of the element. We use it to make the 
+       image size change based on the pet's weight property.
+    */
+    var dynamicWidth = 150 + (pet_info.weight * 2);
+    $('.pet-image').width(dynamicWidth);
+}
